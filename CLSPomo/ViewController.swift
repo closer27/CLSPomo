d970a33000ee8f608b416fe4b584d0f88b8ec387
Seungwon Kim <closer527@gmail.com>
2018-07-11 23:58:30 +0900

    set UI components for showing time remaining and add logic for reflecting time changing #1

--- CLSPomo/ViewController.swift
+++ CLSPomo/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
+    let disposeBag = DisposeBag()
+    let pomoTimer = CLSPomoTimer()
+    @IBOutlet weak var timeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        pomoTimer.secondPerWork = 5
+        pomoTimer.secondPerBreak = 3
+        pomoTimer.secondPerLongRest = 6
+
+        pomoTimer.currentTime.asObservable()
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { (timeInterval) in
+                print(timeInterval)
+                self.timeLabel.text = self.convertTimeForRemaining(timeInterval, total: self.pomoTimer.secondPerWork)
+            }, onError: { (error) in
+                print(error)
+            }).disposed(by: disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func pomoButtonPressed(_ sender: UIButton) {
+        pomoTimer.startPomo()
+    }
+    
+    private func convertTimeForRemaining(_ time: TimeInterval, total: TimeInterval) -> String {
+        let remainTime = total - time
+        let hours = Int(floor(remainTime / 60))
+        let minutes = Int(remainTime) % 60
+        return String(format: "%02d:%02d", hours, minutes)
+    }
 }
 
